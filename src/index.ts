853dab86796cfd15ae88425632523be601fc857a
hafemann <hafemann@sipgate.de>
2024-04-30 19:45:24 +0200

    :alembic: add first github search logic

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,31 @@
 import { getInput, setFailed, setOutput } from '@actions/core';
+import { context } from '@actions/github';
+import { GithubApi } from './api/github';
 
-try {
-  const todoKeyword = getInput('todo-keyword');
-  const commentStyle = getInput('comment-style');
-  const token = getInput('github-token');
-  console.log(
-    `Starting todo action with todo-keyword: ${todoKeyword} and comment-style: ${commentStyle}`,
-  );
+const run = async (): Promise<void> => {
+  try {
+    //gathering facts
+    //todo improve something
+    const todoKeyword = getInput('todo-keyword');
+    const commentStyle = getInput('comment-style');
+    const token = getInput('github-token');
+    const repository = context.repo.repo;
 
-  setOutput('code-snippets', 'hello world');
-  //   const payload = JSON.stringify(context.payload, undefined, 2);
-  //   console.log(`The event payload: ${payload}`);
-} catch (error: unknown) {
-  setFailed('something went wrong');
-}
+    console.log(
+      `Starting todo action with todo-keyword: ${todoKeyword} and comment-style: ${commentStyle} in repository: ${repository}`,
+    );
+
+    //fetching data from github
+    const githubApi = new GithubApi(token);
+
+    const searchResults = await githubApi.searchTodoInRepository(repository);
+
+    console.log('searchResults', searchResults);
+
+    setOutput('code-snippets', 'hello world');
+  } catch (error: unknown) {
+    setFailed('something went wrong');
+  }
+};
+
+run();
